b3d81b44166d1c9b346320b18ca8caccb78a3618
shirleyzero4 <shirley.c.nguyen@gmail.com>
2023-10-01 19:20:34 -0700

    Updated image mobile view

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -48,16 +48,18 @@ export default function Project() {
                                 <span className="project-title">Portfolio Website</span>
 
                                 <div className="project-description">
-                                    blah blah blah..
+                                    The very website you are viewing right now built using React.js, HTML/CSS, and JavaScript. Used to showcase my background along with personal projects.
                                 </div>
 
                                 <div className="project-stack">
-                                    this that this
+                                    <p>VS Code</p>
+                                    <p>React.js</p>
+                                    <p>JavaScript</p>
                                 </div>
 
                                 <div className="project-links">
-                                    <i class="uil uil-github-alt project-link-icon"></i>
-                                    <i class="uil uil-external-link-alt project-link-icon"></i>
+                                    <a href="https://github.com/shirleyzero4/react-personal-site" target="_blank"><i class="uil uil-github-alt project-link-icon"></i></a>
+                                    <a href="https://shirleyzero4.github.io/react-personal-site/" target="_blank"><i class="uil uil-external-link-alt project-link-icon"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -66,7 +68,29 @@ export default function Project() {
                     <div className={toggleState === 2 
                         ? "project-content project-content-active" 
                         : "project-content" }>
-                        <div>Temp Block 2</div>
+                        <div className="project-card container grid">
+                            <img src={WebImg1} className="project-img"/>
+
+                            <div className="project-card-des">
+                                <h3 className="featured-project">Featured Project</h3>
+                                <span className="project-title">Portfolio Website</span>
+
+                                <div className="project-description">
+                                    blah blah blah..
+                                </div>
+
+                                <div className="project-stack">
+                                    <p>VS Code</p>
+                                    <p>React.js</p>
+                                    <p>JavaScript</p>
+                                </div>
+
+                                <div className="project-links">
+                                    <a href="https://github.com/shirleyzero4/react-personal-site" target="_blank"><i class="uil uil-github-alt project-link-icon"></i></a>
+                                    <a href="https://shirleyzero4.github.io/react-personal-site/" target="_blank"><i class="uil uil-external-link-alt project-link-icon"></i></a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <div className={toggleState === 3
